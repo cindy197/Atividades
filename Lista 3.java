3d5cff5057fab1f7c86a1b508005e0238996f2ce
cindy197 <136206933+cindy197@users.noreply.github.com>
2025-04-01 19:14:27 -0300

    Lista 3.java

---
+++ Lista 3.java
@@ -0,0 +1,35 @@
+// Atividade 1: 1 a 100
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
+
+// Atividade 2: 100 a 1
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 100; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+
+    }
+}
+
+// Atividade 3: 
+
+
